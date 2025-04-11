8b0bf1d91bf7de83492e098785e9b23aef9796cc
Karnika Deore <karnikadeore031104@gmail.com>
2025-04-11 12:29:50 +0530

    user details updated

--- server.js
+++ server.js
@@ -1,21 +1,34 @@
 const express = require("express");
 const app = express();
-require("dotenv").config()
+require("dotenv").config();
 
-app.get('/api/get',(req,res) => {
-    res.send({message: "hello back to nodejs"})
-})
+app.get('/api/get', (req, res) => {
+    res.send({ message: "hello back to nodejs" });
+});
 
-app.get('/api/get_user_details',(req,res) => {
+app.get('/api/get_user_details', (req, res) => {
     res.send({
-        user:{
-            name:"karnika",
-            age:20,
-            contact:12254
-        },env:process.env.NAME
-    })
-})
+        users: [
+            {
+                name: "karnika",
+                age: 20,
+                contact: 12254
+            },
+            {
+                name: "gopikha",
+                age: 20,
+                contact: 345674
+            },
+            {
+                name: "logitha",
+                age: 19,
+                contact: 87254
+            }
+        ],
+        env: process.env.NAME
+    });
+});
 
-app.listen(process.env.PORT,() => {
+app.listen(process.env.PORT, () => {
     console.log("listening to 8000");
-})
+});
